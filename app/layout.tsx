f24bedc670ca1ab36bc9fa139d8b609f7b2824e6
Sebastián García <johancs.mm@gmail.com>
2024-06-02 12:22:54 -0500

    Refactor navigation at docs (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import './global.css'
 import '@fontsource-variable/onest'
 import { RootProvider } from 'fumadocs-ui/provider'
 import type { ReactNode } from 'react'
-import Menu from '@/components/navbar'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
         <RootProvider>
-          <Menu />
           {children}
         </RootProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Menu from '@/components/navbar'
+
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-    </main>
+    <>
+      <Menu />
+      <main className="flex h-screen flex-col justify-center text-center">
+        <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      </main>
+    </>
   )
 }
