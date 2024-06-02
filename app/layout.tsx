3391afe21f1c464de878b299b8114417d5922d55
castrogarciajs <johancs.mm@gmail.com>
2024-06-01 21:21:33 -0500

    Add UI module and setup eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import type { ReactNode } from 'react';
-
+import './global.css'
+import { RootProvider } from 'fumadocs-ui/provider'
+import type { ReactNode } from 'react'
+import Menu from '@/components/navbar'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <Menu />
+          {children}
+        </RootProvider>
       </body>
     </html>
-  );
+  )
 }
