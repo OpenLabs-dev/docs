5242da503537b81487083f4bf1171043361650f1
Sebastián García <johancs.mm@gmail.com>
2024-06-01 21:24:04 -0500

    Add UI module and setup eslint (#1)

--- app/(projects)/ui/page.tsx
+++ app/(projects)/ui/page.tsx
@@ -1,5 +1,15 @@
-function PageHomeUI() {
-    return (
-<h2>Hello world UI</h2>
-)
+import Home from '@/components/ui/home'
+import Commons from '@/components/ui/comps'
+import ExploreDocs from '@/components/ui/explorer'
+import FeedbackUI from '@/components/ui/feedback'
+
+export default function HomePage() {
+  return (
+    <>
+      <Home />
+      <Commons />
+      <ExploreDocs />
+      <FeedbackUI />
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-import { createPreset } from 'fumadocs-ui/tailwind-plugin';
+import { openui } from '@openlabs/theme'
+import { createPreset } from 'fumadocs-ui/tailwind-plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,6 +9,8 @@ export default {
     './content/**/*.{md,mdx}',
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
+    './node_modules/@openlabs/theme/dist/**/*.{js,ts,tsx,jsx}',
   ],
   presets: [createPreset()],
-};
+  plugins: [openui()],
+}
