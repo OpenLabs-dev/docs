9d62ba12d54ab1c4ef6a1ff21e2f1f0af9f67bc1
Sebastián García <johancs.mm@gmail.com>
2024-06-03 20:07:05 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import Menu from '@/components/navbar'
+import Link from "next/link"
+import { Button } from "@openlabs/ui"
 
 export default function HomePage() {
   return (
     <>
       <Menu />
-      <main className="flex h-screen flex-col justify-center text-center">
-        <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      </main>
+       <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+      <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
+        <div className="space-y-3">
+          <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Join our waitlist</h2>
+          <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            Be the first to know when we launch.
+          </p>
+        </div>
+        <div className="mx-auto w-full max-w-sm space-y-2">
+          <div className="flex flex-col items-center space-y-2">
+            <Button className="w-full" asChild>
+              <Link href="https://github.com/OpenLabs-dev">
+                Github
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
